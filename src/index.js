500ffed86879dfbe78da5030fcd2bf6ddb081ae5
Pablo Balbi <pablo.l.balbi@gmail.com>
2022-05-24 21:51:15 -0300

    next is now main

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import m from "mithril";
-import Home from "./pages/Home";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-m.route(document.body, "/", {
-  "/": new Home(),
-});
